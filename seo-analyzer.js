6ed08406ddff97ce482f96ef28f8c3c48df39d1e
anky1904 <login.ankushsable@gmail.com>
2026-04-14 11:06:44 +0530

    Update seo-analyzer.js

--- seo-analyzer.js
+++ seo-analyzer.js
@@ -1,77 +1,82 @@
 async function fetchHTML(url){
 
 if(!url.startsWith("http")){
-url = "https://" + url;
+url="https://"+url
 }
 
-const proxy = "https://corsproxy.io/?";
+const proxy="https://corsproxy.io/?"
 
-const response = await fetch(proxy + encodeURIComponent(url));
-const html = await response.text();
+const response=await fetch(proxy+encodeURIComponent(url))
+const html=await response.text()
 
-return html;
+return html
 
 }
+
 
 function parseHTML(html){
 
-const parser = new DOMParser();
-return parser.parseFromString(html, "text/html");
+const parser=new DOMParser()
+return parser.parseFromString(html,"text/html")
 
 }
 
+
 function analyzePage(doc){
 
-const title = doc.querySelector("title")?.innerText || "Missing";
-const meta = doc.querySelector('meta[name="description"]')?.content || "Missing";
+const title=doc.querySelector("title")?.innerText || "Missing"
+const meta=doc.querySelector('meta[name="description"]')?.content || "Missing"
 
-const titleLength = title.length;
-const metaLength = meta.length;
+const titleLength=title.length
+const metaLength=meta.length
 
-const h1 = doc.querySelectorAll("h1");
-const h2 = doc.querySelectorAll("h2");
-const h3 = doc.querySelectorAll("h3");
 
-let h1List="";
-let h2List="";
-let h3List="";
+const h1=doc.querySelectorAll("h1")
+const h2=doc.querySelectorAll("h2")
+const h3=doc.querySelectorAll("h3")
 
-h1.forEach(el=> h1List += `<li>${el.innerText}</li>`);
-h2.forEach(el=> h2List += `<li>${el.innerText}</li>`);
-h3.forEach(el=> h3List += `<li>${el.innerText}</li>`);
 
-let suggestions="";
+let h1List=""
+let h2List=""
+let h3List=""
 
-const titleMissing = title === "Missing";
-const titleLong = titleLength > 70;
+h1.forEach(el=>h1List+=`<li>${el.innerText}</li>`)
+h2.forEach(el=>h2List+=`<li>${el.innerText}</li>`)
+h3.forEach(el=>h3List+=`<li>${el.innerText}</li>`)
 
-const metaMissing = meta === "Missing";
-const metaLong = metaLength > 160;
 
-const h1Missing = h1.length === 0;
-const multipleH1 = h1.length > 1;
+const images=doc.querySelectorAll("img")
 
-if(titleMissing) suggestions += "<li>Missing Meta Title</li>";
-if(titleLong) suggestions += "<li>Meta Title too long</li>";
-if(metaMissing) suggestions += "<li>Missing Meta Description</li>";
-if(metaLong) suggestions += "<li>Meta Description too long</li>";
-if(h1Missing) suggestions += "<li>Missing H1</li>";
-if(multipleH1) suggestions += "<li>Multiple H1 tags</li>";
+let imagesMissingAlt=0
 
-return {
+images.forEach(img=>{
+if(!img.alt){
+imagesMissingAlt++
+}
+})
+
+
+const links=doc.querySelectorAll("a")
+
+let internalLinks=0
+let externalLinks=0
+
+links.forEach(link=>{
+if(link.href.includes(location.hostname)){
+internalLinks++
+}else{
+externalLinks++
+}
+})
+
+
+return{
 
 title,
 meta,
 
 titleLength,
 metaLength,
-
-titleMissing,
-titleLong,
-metaMissing,
-metaLong,
-h1Missing,
-multipleH1,
 
 h1Count:h1.length,
 h2Count:h2.length,
@@ -81,8 +86,13 @@ h1List,
 h2List,
 h3List,
 
-suggestions
+imageCount:images.length,
+imagesMissingAlt,
 
-};
+totalLinks:links.length,
+internalLinks,
+externalLinks
+
+}
 
 }
