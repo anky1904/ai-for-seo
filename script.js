52408008b34c767ee98ab34f6e7bb8047d9d1fa8
anky1904 <login.ankushsable@gmail.com>
2026-04-14 14:45:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,17 @@
-function openTab(evt, tabName){
+function openTab(evt,tabName){
 
-const tabs=document.querySelectorAll(".tab")
-const contents=document.querySelectorAll(".tab-content")
+document.querySelectorAll(".tab-content")
+.forEach(tab=>{
+tab.style.display="none"
+})
 
-tabs.forEach(tab=>{
+document.querySelectorAll(".tab")
+.forEach(tab=>{
 tab.classList.remove("active")
 })
 
-contents.forEach(content=>{
-content.style.display="none"
-})
-
 document.getElementById(tabName).style.display="block"
-
-if(evt){
 evt.currentTarget.classList.add("active")
-}
 
 }
 
@@ -25,7 +21,7 @@ async function analyzeSEO(){
 const url=document.getElementById("urlInput").value
 
 if(!url){
-alert("Enter Website URL")
+alert("Enter URL")
 return
 }
 
@@ -36,19 +32,10 @@ const data=await analyzePage(doc,url)
 
 displayResults(data)
 
-document.querySelectorAll(".tab").forEach(tab=>{
-tab.classList.remove("active")
-})
-
-document.querySelector(".tab").classList.add("active")
-
-document.querySelectorAll(".tab-content").forEach(content=>{
-content.style.display="none"
-})
-
-document.getElementById("meta").style.display="block"
+document.querySelector(".tab").click()
 
 }
+
 
 
 function displayResults(data){
@@ -72,45 +59,22 @@ Overall SEO Score
 `
 
 
+
 document.getElementById("meta").innerHTML=`
 
-<div class="card">
-
 <h3>Meta Title</h3>
-
-<p>${data.titleHighlighted}</p>
-
-<p>${data.titleLength}/70</p>
+<p>${data.title}</p>
 
 <h3>Meta Description</h3>
-
-<p>${data.metaHighlighted}</p>
-
-<p>${data.metaLength}/160</p>
-
-</div>
+<p>${data.meta}</p>
 
 `
 
 
-document.getElementById("headers").innerHTML=`
-
-<div class="card">
-
-<h3>Heading Structure</h3>
-
-${data.headingStructure}
-
-</div>
-
-`
+document.getElementById("headers").innerHTML=data.headingStructure
 
 
 document.getElementById("images").innerHTML=`
-
-<div class="card">
-
-<h3>Images</h3>
 
 <p>Total Images: ${data.imageCount}</p>
 
@@ -129,48 +93,28 @@ ${data.imagesMissingAltList}
 
 </table>
 
-</div>
-
 `
 
 
 document.getElementById("links").innerHTML=`
 
-<div class="card">
-
 <p>Total Links: ${data.totalLinks}</p>
 <p>Internal Links: ${data.internalLinks}</p>
 <p>External Links: ${data.externalLinks}</p>
-
-</div>
 
 `
 
 
 document.getElementById("technical").innerHTML=`
 
-<div class="card">
-
 <p>Canonical: ${data.canonical}</p>
 <p>Robots: ${data.robots}</p>
 <p>Schema: ${data.schema}</p>
 <p>Sitemap: ${data.sitemap}</p>
 
-</div>
-
 `
 
 
-document.getElementById("suggestions").innerHTML=`
-
-<div class="card">
-
-<h3>SEO Suggestions</h3>
-
-${data.suggestions}
-
-</div>
-
-`
+document.getElementById("suggestions").innerHTML=data.suggestions
 
 }
